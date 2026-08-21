fe93a9bf6cad20620241d9552be6e9e1b8177eb1
WONSH1N-PTNC <WONSH1N-PTNC@users.noreply.github.com>
2026-08-21 01:53:42 +0000

    deploy: b29e77bdae993db8b620c3896f1502eb019f4282

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3652122615" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "47626044" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
